a5af6cfb916835909572dfad9bc45618f0fd4bb9
DEV-COM <kwp@tlen.pl>
2023-12-19 12:14:59 +0100

    12:14

--- start_new_t.py
+++ start_new_t.py
@@ -1,15 +1,24 @@
 from threading import Thread
 
+thread_visits = 0
 
-def my_func():
-    print("Drukuje z threada")
+
+def visit_counter():
+    global thread_visits
+    for i in range(100_000):
+        value = thread_visits
+        thread_visits = value + 1
 
 
 if __name__ == '__main__':
-    threads = [Thread(target=my_func) for _ in range(10)]
+    thread_count = 100
+    threads = [
+        Thread(target=visit_counter) for _ in range(thread_count)]
 
     for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
+
+    print(f"{thread_count=},{thread_visits=}")
